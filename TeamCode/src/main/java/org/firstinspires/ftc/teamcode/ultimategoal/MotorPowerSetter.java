b7d0d96e2d7a9596b29cc7d9814161658e25cb93
airsquared <36649395+airsquared@users.noreply.github.com>
2021-01-02 16:24:08 -0800

    Remove duplicated files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/MotorPowerSetter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/MotorPowerSetter.java
@@ -2,16 +2,14 @@ package org.firstinspires.ftc.teamcode.ultimategoal;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.OpMode8872;
 
 @TeleOp
 @Config
-
 public class MotorPowerSetter extends UltimateGoalOpMode {
 
-    public int RPM1 = 0;
-    public int RPM2 = 0;
-    public String MotorChangingSetting = "Motor1";
+    public int rpm1 = 0;
+    public int rpm2 = 0;
+    public String motorChangingSetting = "Motor1";
     public String name1 = "(Config) shooter1 Motor Power: ";
     public String name2 = "shooter2 Motor Power: ";
 
@@ -21,53 +19,54 @@ public class MotorPowerSetter extends UltimateGoalOpMode {
     private boolean lastAState1 = false;
     private boolean lastYState1 = false;
     private boolean lastXState1 = false;
+
     @Override
-    public void loop(){
-        telemetry.addLine().addData(name1, () -> RPM1);
-        telemetry.addLine().addData(name2, () -> RPM2);
+    public void loop() {
+        telemetry.addLine().addData(name1, () -> rpm1);
+        telemetry.addLine().addData(name2, () -> rpm2);
 
         // Change these two motors based on where you have it plugged in
-        shooter1.setPower(RPM1 / 6000.0);
-        shooter2.setPower(RPM2 / 6000.0);
+        shooter1.setPower(rpm1 / 6000.0);
+        shooter2.setPower(rpm2 / 6000.0);
 
-        if (gamepad1.x && !lastXState1 & MotorChangingSetting.equals("Motor1")) {
-            MotorChangingSetting = "Motor2";
-            name2  = "(Config) " + name2;
+        if (gamepad1.x && !lastXState1 & motorChangingSetting.equals("Motor1")) {
+            motorChangingSetting = "Motor2";
+            name2 = "(Config) " + name2;
             name1 = "shooter1 Motor Power: ";
 
-        }else if(gamepad1.x && !lastXState1) {
-            MotorChangingSetting = "Motor1";
+        } else if (gamepad1.x && !lastXState1) {
+            motorChangingSetting = "Motor1";
             name1 = "(Config) " + name1;
             name2 = "shooter2 Motor Power: ";
         }
         lastXState1 = gamepad1.x;
 
 
-        if (MotorChangingSetting.equals("Motor1")) {
-            RPM1 = MotorPowerSetting(RPM1);
+        if (motorChangingSetting.equals("Motor1")) {
+            rpm1 = motorPowerSetting(rpm1);
         } else {
-            RPM2 = MotorPowerSetting(RPM2);
+            rpm2 = motorPowerSetting(rpm2);
         }
 
-        if(MotorChangingSetting.equals("Motor1")){
-            RPM1 += Math.round(gamepad1.left_stick_y * stickChange);
-        }else{
-            RPM2 += Math.round(gamepad1.left_stick_y * stickChange);
+        if (motorChangingSetting.equals("Motor1")) {
+            rpm1 += Math.round(gamepad1.left_stick_y * stickChange);
+        } else {
+            rpm2 += Math.round(gamepad1.left_stick_y * stickChange);
         }
     }
 
-    public int MotorPowerSetting(int RPM){
-        if (gamepad1.a && !lastAState1 && RPM >= buttonChange) {
-            RPM += buttonChange;
+    public int motorPowerSetting(int rpm) {
+        if (gamepad1.a && !lastAState1 && rpm >= buttonChange) {
+            rpm += buttonChange;
         }
         lastAState1 = gamepad1.a;
 
-        if (gamepad1.y && !lastYState1 && RPM >= buttonChange) {
-            RPM -= buttonChange;
+        if (gamepad1.y && !lastYState1 && rpm >= buttonChange) {
+            rpm -= buttonChange;
         }
         lastYState1 = gamepad1.y;
 
-        return RPM;
+        return rpm;
     }
 }
 
